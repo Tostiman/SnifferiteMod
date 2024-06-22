6bbeaf6c45226d5825b7e91c97555d0f8ce1279b
Tostiman <tostimannetje@gmail.com>
2024-06-22 16:49:20 +0200

    v1.0-1.20.3

--- src/main/java/net/tostiman/snifferite/block/SplashBerryBushBlock.java
+++ src/main/java/net/tostiman/snifferite/block/SplashBerryBushBlock.java
@@ -1,5 +1,6 @@
 package net.tostiman.snifferite.block;
 
+import com.mojang.serialization.MapCodec;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Fertilizable;
@@ -33,19 +34,25 @@ import net.tostiman.snifferite.item.ModItems;
 
 public class SplashBerryBushBlock extends PlantBlock implements Fertilizable {
 
-	private static final float HURT_SPEED_THRESHOLD = 0.003f;
+	public static final MapCodec<SplashBerryBushBlock> CODEC = SplashBerryBushBlock.createCodec(SplashBerryBushBlock::new);
+	private static final float MIN_MOVEMENT_FOR_DAMAGE = 0.003f;
     public static final int MAX_AGE = 3;
     public static final IntProperty AGE = Properties.AGE_3;
     private static final VoxelShape SMALL_SHAPE = Block.createCuboidShape(3.0, 0.0, 3.0, 13.0, 8.0, 13.0);
     private static final VoxelShape LARGE_SHAPE = Block.createCuboidShape(1.0, 0.0, 1.0, 15.0, 16.0, 15.0);
 	
+    @Override
+	protected MapCodec<? extends PlantBlock> getCodec() {
+		return CODEC;
+	}
+    
 	public SplashBerryBushBlock(Settings settings) {
 		super(settings);
 		this.setDefaultState((BlockState)((BlockState)this.stateManager.getDefaultState()).with(AGE, 0));
 	}
 
 	@Override
-    public ItemStack getPickStack(BlockView world, BlockPos pos, BlockState state) {
+    public ItemStack getPickStack(WorldView world, BlockPos pos, BlockState state) {
         return new ItemStack(ModItems.itemSplashBerries);
     }
 
@@ -80,7 +87,7 @@ public class SplashBerryBushBlock extends PlantBlock implements Fertilizable {
         if (!(world.isClient || state.get(AGE) <= 0 || entity.lastRenderX == entity.getX() && entity.lastRenderZ == entity.getZ())) {
             double deltaX = Math.abs(entity.getX() - entity.lastRenderX);
             double deltaZ = Math.abs(entity.getZ() - entity.lastRenderZ);
-            if (deltaX >= (double)HURT_SPEED_THRESHOLD || deltaZ >= (double)HURT_SPEED_THRESHOLD) {
+            if (deltaX >= (double)MIN_MOVEMENT_FOR_DAMAGE || deltaZ >= (double)MIN_MOVEMENT_FOR_DAMAGE) {
                 entity.damage(world.getDamageSources().sweetBerryBush(), 1.0f);
             }
         }
